4ccaad27b7aa44df68d6a13283dac836d5d98bb7
Le <MinhHieu.Le@aia.com>
2021-03-08 18:03:41 +0700

    Initial commit.

---
+++ src/main/java/io/hieu/imageuploadingapi/ImageUploadingApiApplication.java
@@ -0,0 +1,13 @@
+package io.hieu.imageuploadingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageUploadingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageUploadingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/hieu/imageuploadingapi/ImageUploadingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hieu.imageuploadingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageUploadingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
