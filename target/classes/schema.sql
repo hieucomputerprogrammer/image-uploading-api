cb65824f39c051a1fe2b4f924fc14c3bcc28cfa0
Le <MinhHieu.Le@aia.com>
2021-03-10 16:19:32 +0700

    Updated .gitignore to not ignore files under /target directory to be able to push the Uber/Fat JAR to Git repo.

---
+++ target/classes/schema.sql
@@ -0,0 +1,19 @@
+-- H2:
+CREATE TABLE image(
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    image_as_base64_format BYTEA NOT NULL,
+    image_title VARCHAR(255) NOT NULL,
+    owner_name VARCHAR(255) NOT NULL,
+    owner_phone_number VARCHAR(255),
+    owner_email VARCHAR(255)
+);
+
+-- POSTGRESQL:
+-- CREATE TABLE IF NOT EXISTS image(
+--     id SERIAL PRIMARY KEY,
+--     image_as_base64_format BYTEA NOT NULL,
+--     image_title VARCHAR(255) NOT NULL,
+--     owner_name VARCHAR(255) NOT NULL,
+--     owner_phone_number VARCHAR(255),
+--     owner_email VARCHAR(255)
+-- );
