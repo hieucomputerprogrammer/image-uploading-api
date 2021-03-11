fbd5d222b7eee3f762a19cb4f7670a0e70ef5260
Le <MinhHieu.Le@aia.com>
2021-03-11 16:24:03 +0700

    Added website icon and updated Image Controller.

--- src/main/java/io/hieu/imagesapi/ImagesApiApplication.java
+++ src/main/java/io/hieu/imagesapi/ImagesApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ImagesApiApplication {
-    private Logger logger = LoggerFactory.getLogger(ImagesApiApplication.class);
+    private final Logger logger = LoggerFactory.getLogger(ImagesApiApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(ImagesApiApplication.class, args);
--- src/main/java/io/hieu/imagesapi/web/controller/ImageController.java
+++ src/main/java/io/hieu/imagesapi/web/controller/ImageController.java
@@ -1,6 +1,8 @@
 package io.hieu.imagesapi.web.controller;
 
 import io.hieu.imagesapi.service.ImageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/images")
 public class ImageController {
+    private final Logger logger = LoggerFactory.getLogger(ImageController.class);
     private final ImageService imageService;
 
     @Autowired
@@ -16,8 +19,14 @@ public class ImageController {
         this.imageService = imageService;
     }
 
-    @GetMapping("/open-api")
+    @GetMapping({"/open-api", "/swagger"})
     public String openApi() {
+        this.logger.info("INFO: Image Controller - openApi() method called. Redirecting to /swagger-ui.html endpoint.");
+        this.logger.debug("DEBUG: Image Controller - openApi() method called. Redirecting to /swagger-ui.html endpoint.");
+        this.logger.trace("TRACE: Image Controller - openApi() method called. Redirecting to /swagger-ui.html endpoint.");
+        this.logger.warn("WARN: Image Controller - openApi() method called. Redirecting to /swagger-ui.html endpoint.");
+        this.logger.error("ERROR: Image Controller - openApi() method called. Redirecting to /swagger-ui.html endpoint.");
+
         return "redirect:/swagger-ui.html";
     }
 }
