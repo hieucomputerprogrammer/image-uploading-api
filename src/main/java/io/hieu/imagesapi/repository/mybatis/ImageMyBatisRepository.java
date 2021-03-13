44f72fa65b9bf194118b47a62613b804efd0da3e
Le <MinhHieu.Le@aia.com>
2021-03-13 20:03:52 +0700

    Implementing converting Base64 code to Image file and store it in local directory and implementing Unit Tests using JUnit and Mockito.

--- src/main/java/io/hieu/imagesapi/repository/mybatis/ImageMyBatisRepository.java
+++ src/main/java/io/hieu/imagesapi/repository/mybatis/ImageMyBatisRepository.java
@@ -10,7 +10,7 @@ public interface ImageMyBatisRepository {
     @Insert("INSERT INTO image(image_as_base64_format, image_title, owner_name, owner_phone_number, owner_email) " +
             "VALUES(#{imageAsBase64Format}, #{imageTitle}, #{ownerName}, #{ownerPhoneNumber}, #{ownerEmail})")
     @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
-    int insert(Image image);
+    Image insert(Image image);
 
     @Select("SELECT id AS id, image_as_base64_format AS imageAsBase64Format, image_title AS imageTitle, owner_name AS ownerName, owner_phone_number AS ownerPhoneNumber, owner_email AS ownerEmail " +
             "FROM image")
@@ -96,7 +96,7 @@ public interface ImageMyBatisRepository {
             "owner_phone_number=#{image.ownerPhoneNumber}, " +
             "owner_email=#{image.ownerEmail} " +
             "WHERE id=#{image.id}")
-    int update(Long id, Image image);
+    Image update(Long id, Image image);
 
     @Delete("DELETE FROM image")
     int deleteAll();
--- src/test/java/io/hieu/imagesapi/repository/mybatis/ImageMyBatisRepositoryTest.java
+++ src/test/java/io/hieu/imagesapi/repository/mybatis/ImageMyBatisRepositoryTest.java
@@ -1,10 +1,28 @@
 package io.hieu.imagesapi.repository.mybatis;
 
+import io.hieu.imagesapi.domain.Image;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@DataJpaTest
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class ImageMyBatisRepositoryTest {
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Mock
+    private ImageMyBatisRepository imageMyBatisRepository;
+
     @Before
     public void setUp() throws Exception {
     }
@@ -15,6 +33,15 @@ public class ImageMyBatisRepositoryTest {
 
     @Test
     public void insert() {
+        Image image = new Image(
+                "TESTING".getBytes(),
+                "TEST IMAGE",
+                "Hieu Minh Le",
+                "+84908109633",
+                "hieu.minhle@outlook.com"
+        );
+        Image savedImage = this.imageMyBatisRepository.insert(image);
+        Assert.assertNotNull(savedImage);
     }
 
     @Test
