17086080bea8b375acdc129dfba80f092b33a9ff
Le <MinhHieu.Le@aia.com>
2021-03-13 17:54:04 +0700

    Implemented Base64 Converter under /util package for encoding and decoding images in Base64 format.

---
+++ src/main/java/io/hieu/imagesapi/util/Base64Converter.java
@@ -0,0 +1,21 @@
+package io.hieu.imagesapi.util;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Base64;
+
+public class Base64Converter {
+    public static String imageToBase64(File imageFilePath) throws IOException {
+        byte[] imageFile = FileUtils.readFileToByteArray(imageFilePath);
+        String imageAsBase64String = Base64.getEncoder().encodeToString(imageFile);
+
+        return imageAsBase64String;
+    }
+
+    public static void base64ToImage(String imageAsBase64String, String imageTitle) throws IOException {
+        byte[] image = Base64.getDecoder().decode(imageAsBase64String);
+        FileUtils.writeByteArrayToFile(new File(imageTitle), image);
+    }
+}
